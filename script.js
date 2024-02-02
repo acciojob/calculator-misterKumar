6f8e0788f95079344d60f98232a13843c3137eb1
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-02 08:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your code here
+let displayValue = '';
+
+function updateDisplay(){
+	document.getElementById('input').value = displayValue;
+}
+
+function appendtoDisplay(value){
+	displayValue+=value;
+	updateDisplay();
+}
+
+function clearDisplay(){
+	displayValue = '';
+	updateDisplay();
+}
+
+function clearEntry(){
+	displayValue = displayValue.slice(0,-1);
+	updateDisplay();
+}
+
+function calculate(){
+	try{
+		const result = eval(displayValue);
+		displayValue = isFinite(result) ? result.toString() : 'Infinity';
+		displayValue = isNaN(result) ? 'NaN' : displayValue;
+		updateDisplay();
+	}
+	catch(error){
+		displayValue = 'Error';
+		updateDisplay();
+	}
+}
